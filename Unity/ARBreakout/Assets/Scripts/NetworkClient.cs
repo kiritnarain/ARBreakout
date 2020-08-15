0d96552055f90cb1061236c5ec7cfc821086f97e
kiritnarain <kiritnarain@gmail.com>
2020-08-16 05:28:25 +0530

    Added some networking

--- Unity/ARBreakout/Assets/Scripts/NetworkClient.cs
+++ Unity/ARBreakout/Assets/Scripts/NetworkClient.cs
@@ -1,18 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SocketIO;
 
-public class NetworkClient : MonoBehaviour
+public class NetworkClient : SocketIOComponent
 {
+    public GameObject PlayerModel;
+    private Dictionary<string, GameObject> playerObjects;
+    private string id = null;
+     
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
+    }
+
+    private void setupEvents()
+    {
+        On("open", (E) =>
+        {
+            Debug.Log("Connection made");
+        });
+
+        On("register", (E) =>{
+            Debug.Log("Got register server response");
+            JSONObject data = E.data;
+            string id = cleanJSONStr(data["id"].ToString());
+            this.id = id;
+            JSONObject syncObj = new JSONObject(JSONObject.Type.OBJECT);
+            syncObj.AddField("name", "Player One");
+            Emit("sync", syncObj);
+        });
+
+        On("updatePosition", (E) => {
+            List<JSONObject> positionsList = E.data.list;
+            updatePlayerPositions(positionsList);
+
+        });
+    }
+
+    private void updatePlayerPositions(List<JSONObject> positionsList)
+    {
+        foreach(JSONObject obj in positionsList)
+        {
+            string id = cleanJSONStr(obj["id"].ToString());
+            if(id == getID())
+            {
+                continue;
+            }
+            if (playerObjects.ContainsKey(id))
+            {
+                //Update position
+            }
+            else
+            {
+                //Set new Player
+                GameObject nplayer = Instantiate(PlayerModel);
+            }
+        }
+    }
+
+    private string cleanJSONStr(string item)
+    {
+        if (item[0] == '\"')
+        {
+            return item.Substring(1, item.Length - 2);
+        }
+        return item;
+    }
+
+    public string getID()
+    {
+        return this.id;
     }
 }
